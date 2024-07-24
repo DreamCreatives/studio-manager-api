6eb510b7d90f4dc03c4cc7219076f97245c448d4
Kamil Oberaj <kamil.oberaj@gmail.com>
2024-07-24 21:53:27 +0200

    fix: add missing Delete user endpoint

--- StudioManager.API/Controllers/V1/UsersController.cs
+++ StudioManager.API/Controllers/V1/UsersController.cs
@@ -4,6 +4,7 @@ using StudioManager.API.Base;
 using StudioManager.API.Contracts.Pagination;
 using StudioManager.API.Contracts.Users;
 using StudioManager.Application.Users.Create;
+using StudioManager.Application.Users.Delete;
 using StudioManager.Application.Users.GetAll;
 using StudioManager.Application.Users.GetById;
 using StudioManager.Application.Users.Update;
@@ -25,6 +26,10 @@ public sealed class UsersController(ISender sender) : CoreController(sender)
     [HttpGet("{id:guid}")]
     [ProducesResponseType<UserReadDto>(StatusCodes.Status200OK)]
     public async Task<IResult> GetByIdAsync(Guid id) => await SendAsync(new GetUserByIdQuery(id));
+    
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType<CommandResult>(StatusCodes.Status200OK)]
+    public async Task<IResult> DeleteUserAsync(Guid id) => await SendAsync(new DeleteUserCommand(id));
     
     [HttpGet]
     [ProducesResponseType<PagingResultDto<UserReadDto>>(StatusCodes.Status200OK)]
