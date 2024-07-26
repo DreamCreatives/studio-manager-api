18a67686db23138762f65abacdd35fb3ee91c318
Kamil Oberaj <kamil.oberaj@gmail.com>
2024-07-26 22:51:23 +0200

    fix: claims in token decryptor

--- StudioManager.API/Common/TokenDecryptor.cs
+++ StudioManager.API/Common/TokenDecryptor.cs
@@ -1,24 +1,24 @@
-﻿using System.Security.Claims;
-using StudioManager.Application.Common;
+﻿using StudioManager.Application.Common;
 
 namespace StudioManager.API.Common;
 
 public sealed class TokenDecryptor(IHttpContextAccessor httpContextAccessor) : ITokenDecryptor
 {
-    private const string ClientIdKey = "ezp";
+    private const string ClientIdKey = "azp";
+    private const string UserIdKey = "sub";
+    private string? _clientId;
 
     private string? _userId;
-    private string? _clientId;
 
     public string? UserId => GetUserId();
     public string? ClientId => GetClientId();
-    
+
     private string? GetUserId()
     {
-        GetClaimValue(ClaimTypes.NameIdentifier, ref _userId);
+        GetClaimValue(UserIdKey, ref _userId);
         return _userId;
     }
-    
+
     private string? GetClientId()
     {
         GetClaimValue(ClientIdKey, ref _clientId);
@@ -33,10 +33,12 @@ public sealed class TokenDecryptor(IHttpContextAccessor httpContextAccessor) : I
         }
 
         var userData = httpContextAccessor.HttpContext?.User.Identity;
-        
+
         if (userData is { IsAuthenticated: true })
         {
-            var claimValue = httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(x => x.Type.Equals(claim, StringComparison.InvariantCultureIgnoreCase));
+            var claimValue =
+                httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(x =>
+                    x.Type.Equals(claim, StringComparison.InvariantCultureIgnoreCase));
             value = claimValue?.Value;
         }
     }
